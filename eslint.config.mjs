e2abf3360b2f42e8223b83bced057b10077997d2
KishanJ22 <kishanjethwa224@gmail.com>
2025-08-15 20:24:29 +0100

    add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import recommendedConfig from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
+import react from "eslint-plugin-react";
+
+export default [
+    eslintConfigPrettier,
+    recommendedConfig,
+    eslint.configs.recommended,
+    ...tseslint.configs.stylistic,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
+        plugins: {
+            import: importPlugin,
+            react,
+        },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-require-imports": "warn",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "no-unused-vars": "off",
+            "prettier/prettier": "warn",
+            "import/extensions": [
+                "error",
+                "ignorePackages",
+                {
+                    js: "always",
+                    ts: "never",
+                    tsx: "never",
+                    jsx: "never",
+                },
+            ],
+        },
+    },
+];
