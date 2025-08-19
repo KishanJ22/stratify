dc2647a749d0b7dcb901dd366fe11343773ef17b
KishanJ22 <kishanjethwa224@gmail.com>
2025-08-19 23:40:15 +0100

    add health endpoint

--- apps/api/stratify-yahoo-api/src/main.py
+++ apps/api/stratify-yahoo-api/src/main.py
@@ -1,7 +1,7 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.generate_openapi_spec import save_openapi_spec
-from src.config import openapi_config
+from src.config import get_package_version, openapi_config
 from src.routes.routes import router as routes
 
 @asynccontextmanager
@@ -10,8 +10,13 @@ async def lifespan(app: FastAPI):
     yield
     
 app = FastAPI(lifespan=lifespan, **openapi_config)
+
 app.include_router(routes)
 
 @app.get("/", status_code=200)
 def get_root():
     return {"message": "Welcome to Stratify Yahoo API"}
+
+@app.get("/health", status_code=200)
+def get_health():
+    return {"status": "Ok", "version": get_package_version()}
