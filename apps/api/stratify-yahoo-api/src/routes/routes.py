edd24962ea6104be13a572a9f9830cc21b6d1a5b
KishanJ22 <kishanjethwa224@gmail.com>
2025-08-19 23:40:39 +0100

    update endpoint router names

--- apps/api/stratify-yahoo-api/src/routes/routes.py
+++ apps/api/stratify-yahoo-api/src/routes/routes.py
@@ -1,8 +1,8 @@
-from src.routes.stocks.stocks_get import router as stocks_router
-from src.routes.stocks.ticker.ticker_get import router as ticker_router
+from src.routes.stocks.stocks_get import symbols_get
+from src.routes.stocks.symbol.symbol_get import symbol_get
 from fastapi import APIRouter
 
 router = APIRouter()
 
-router.include_router(stocks_router)
-router.include_router(ticker_router)
+router.include_router(symbols_get)
+router.include_router(symbol_get)
