d01ac4aba0c2efbf5d44e94f92f74a7b5677474f
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-05 17:56:58 +0000

    build middleware for proxying API requests to the right server based on the path in the request

---
+++ apps/ui/stratify-ui/middleware.ts
@@ -0,0 +1,58 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const API_URL = process.env.STRATIFY_API_URL;
+const AUTH_API_URL = process.env.STRATIFY_AUTH_API_URL;
+
+const proxyRequest = async (request: NextRequest) => {
+    const requestPathname = request.nextUrl.pathname;
+
+    if (requestPathname.startsWith("/api/health")) {
+        return NextResponse.next();
+    }
+
+    const isAuthRequest = requestPathname.startsWith("/api/auth");
+
+    if (!API_URL) {
+        console.error("STRATIFY_API_URL is not defined");
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+
+    if (isAuthRequest) {
+        if (!AUTH_API_URL) {
+            console.error("STRATIFY_AUTH_API_URL is not defined");
+            return new NextResponse("Internal Server Error", { status: 500 });
+        }
+    }
+
+    try {
+        const { pathname, search } = request.nextUrl;
+        const headers = new Headers(request.headers);
+
+        // Determine the target URL based on the request type
+        const targetBaseUrl = isAuthRequest ? AUTH_API_URL : API_URL;
+        const targetUrl = `${targetBaseUrl}${pathname}${search}`;
+
+        headers.delete("host");
+        headers.delete("connection");
+        headers.delete("x-forwarded-host");
+        headers.delete("x-forwarded-proto");
+        headers.set("x-original-url", request.url);
+
+        return NextResponse.rewrite(targetUrl, {
+            request: {
+                headers,
+            },
+        });
+    } catch (error) {
+        console.error("Proxy error:", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+};
+
+export async function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.startsWith("/api")) {
+        return proxyRequest(request);
+    }
+
+    return NextResponse.next();
+}
