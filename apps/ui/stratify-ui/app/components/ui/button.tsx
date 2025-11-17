f77a20d860f9b4f3ba3ff2ba97c4e82c9ec9bb0d
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-17 00:48:13 +0000

    setup shadcn components

---
+++ apps/ui/stratify-ui/app/components/ui/button.tsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-sans font-medium hover:cursor-pointer transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+    {
+        variants: {
+            variant: {
+                default:
+                    "bg-primary-base text-primary-lightest hover:bg-primary-dark",
+                destructive:
+                    "bg-negative-base text-negative-lightest hover:bg-negative-dark",
+                outline:
+                    "border border-light bg-background shadow-sm hover:bg-muted-lighter hover:text-foreground-light",
+                secondary:
+                    "bg-accent-lighter text-foreground-light border border-secondary-base hover:bg-secondary-base hover:border-secondary-light",
+                ghost: "hover:bg-accent-base hover:text-accent-lightest",
+                link: "text-primary-darkest underline-offset-4 hover:underline",
+            },
+            size: {
+                default: "h-9 px-4 py-2",
+                sm: "h-8 rounded-md px-3 text-xs",
+                lg: "h-10 rounded-md px-8",
+                icon: "h-9 w-9",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default",
+        },
+    },
+);
+
+export interface ButtonProps
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
+    asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, variant, size, asChild = false, ...props }, ref) => {
+        const Comp = asChild ? Slot : "button";
+        return (
+            <Comp
+                className={cn(buttonVariants({ variant, size, className }))}
+                ref={ref}
+                {...props}
+            />
+        );
+    },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
