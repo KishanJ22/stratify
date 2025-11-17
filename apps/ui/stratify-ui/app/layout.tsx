6aa0134ee5af9f981350d537e0a224d1edaa7b11
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-17 00:49:21 +0000

    setup fonts using nextjs font functions

--- apps/ui/stratify-ui/app/layout.tsx
+++ apps/ui/stratify-ui/app/layout.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./global/Providers";
+import Navbar from "./components/Navbar/Navbar";
+import { Commissioner, Kaisei_Decol, Source_Code_Pro } from "next/font/google";
 
 export const metadata: Metadata = {
     title: "Stratify UI",
-    description: "Stratify UI Application",
+    description: "Stratify UI",
 };
+
+const fontSans = Commissioner({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
+
+const fontSerif = Kaisei_Decol({
+    subsets: ["latin"],
+    weight: ["400", "500", "700"],
+});
+
+const fontMono = Source_Code_Pro({
+    subsets: ["latin"],
+    variable: "--font-mono",
+});
 
 interface RootLayoutProps {
     children: React.ReactNode;
@@ -18,11 +35,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
             <head />
-            <body className="antialiased font-sans" id="root">
+            <body
+                className={`${fontSans.className} ${fontSerif.className} ${fontMono.className} antialiased`}
+                id="root"
+            >
                 <Providers
                     apiProxyUrl={apiProxyUrl}
                     authProxyUrl={authProxyUrl}
                 >
+                    <Navbar />
                     {children}
                 </Providers>
             </body>
