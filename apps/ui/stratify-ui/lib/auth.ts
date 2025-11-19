88e4e2c562258517b1aa85ffacb35561902c4f68
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-19 21:17:58 +0000

    move store auth token function to separate file

--- apps/ui/stratify-ui/lib/auth.ts
+++ apps/ui/stratify-ui/lib/auth.ts
@@ -1,7 +1,6 @@
 import { useEnvironmentContext } from "@/app/global/EnvironmentProvider";
 import { usernameClient, jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { cookies } from "next/headers";
 
 export const useAuthClient = () => {
     const {
@@ -12,17 +11,4 @@ export const useAuthClient = () => {
         baseUrl: authProxyUrl,
         plugins: [usernameClient(), jwtClient()],
     });
-};
-
-export const storeAuthToken = async (token: string) => {
-    const cookieStore = await cookies();
-    const authToken = cookieStore.get("bearer-token");
-
-    if (!authToken || authToken.value !== token) {
-        cookieStore.set({
-            name: "bearer-token",
-            value: token,
-            httpOnly: true,
-        });
-    }
 };
