1f9186a108220827a1001ed9f280deeb1bf12e7f
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-19 20:35:29 +0000

    rename auth-client to auth and add function to store opaque bearer token in cookies

--- apps/ui/stratify-ui/lib/auth.ts
+++ apps/ui/stratify-ui/lib/auth.ts
@@ -1,6 +1,7 @@
 import { useEnvironmentContext } from "@/app/global/EnvironmentProvider";
 import { usernameClient, jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import { cookies } from "next/headers";
 
 export const useAuthClient = () => {
     const {
@@ -11,4 +12,17 @@ export const useAuthClient = () => {
         baseUrl: authProxyUrl,
         plugins: [usernameClient(), jwtClient()],
     });
+};
+
+export const storeAuthToken = async (token: string) => {
+    const cookieStore = await cookies();
+    const authToken = cookieStore.get("bearer-token");
+
+    if (!authToken || authToken.value !== token) {
+        cookieStore.set({
+            name: "bearer-token",
+            value: token,
+            httpOnly: true,
+        });
+    }
 };
