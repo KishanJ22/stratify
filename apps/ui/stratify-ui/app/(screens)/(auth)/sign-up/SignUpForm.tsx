03fb0b045a21d30224f3042cf2cc8d172403b9aa
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-19 14:43:51 +0000

    add regex patterns to the password validator to make sure it is secure

--- apps/ui/stratify-ui/app/(screens)/(auth)/sign-up/SignUpForm.tsx
+++ apps/ui/stratify-ui/app/(screens)/(auth)/sign-up/SignUpForm.tsx
@@ -13,7 +13,20 @@ const signUpSchema = zod
         email: zod.email({ error: "Email should be set" }),
         password: zod
             .string()
-            .min(8, "Password must be at least 8 characters long"),
+            .min(8, "Password must be at least 8 characters long")
+            .regex(
+                /[A-Z]/,
+                "Password must contain at least one uppercase letter",
+            )
+            .regex(
+                /[a-z]/,
+                "Password must contain at least one lowercase letter",
+            )
+            .regex(/[0-9]/, "Password must contain at least one number")
+            .regex(
+                /[!@#$%^&*(),.?":{}|<>]/,
+                "Password must contain at least one special character",
+            ),
         confirmPassword: zod.string(),
     })
     .refine(({ password, confirmPassword }) => password === confirmPassword, {
@@ -69,9 +82,7 @@ const SignUpForm = () => {
                     username: value.username,
                 });
 
-                toast.success("Account created successfully!", {
-                    className: "text-positive-base",
-                });
+                toast.success("Account created successfully!");
 
                 setIsSubmitProcessing(false);
             } else {
