ad7832841b83d923aa2c49c8dbe7f2dc0d81dc0b
KishanJ22 <kishanjethwa224@gmail.com>
2025-11-23 22:03:56 +0000

    add app route group for routes dependent on being signed in and build session context provider

---
+++ apps/ui/stratify-ui/app/(screens)/app/SessionProvider.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useAuthClient } from "@/lib/auth/auth";
+import { getSessionTokenFromCookies } from "@/lib/auth/get-auth-token";
+import { getUserSession } from "@/lib/auth/get-session";
+import { logoutUser } from "@/lib/auth/logout";
+import { useRouter } from "next/navigation";
+import {
+    createContext,
+    Dispatch,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+} from "react";
+
+export interface UserSession {
+    token: string;
+    userDetails: {
+        id: string;
+        name: string;
+        username: string;
+        displayUsername: string;
+        email: string;
+    };
+}
+
+interface SessionContextType {
+    session: UserSession | null;
+    setSession: Dispatch<SetStateAction<UserSession | null>>;
+    logout: () => void;
+}
+
+export const SessionContext = createContext<SessionContextType | null>(null);
+
+interface SessionProviderProps {
+    children: React.ReactNode;
+}
+
+export const SessionProvider = ({ children }: SessionProviderProps) => {
+    const [session, setSession] = useState<UserSession | null>(null);
+
+    const authClient = useAuthClient();
+    const { push } = useRouter();
+
+    const logout = async () => {
+        const bearer = await getSessionTokenFromCookies();
+
+        if (bearer) {
+            await logoutUser(bearer, authClient);
+
+            //? loggedOut will allow for showing a logged out message on the login page
+            setSession(null);
+            push("/login?loggedOut=true");
+        }
+    };
+
+    useEffect(() => {
+        const loadSession = async () => {
+            const bearer = await getSessionTokenFromCookies();
+
+            if (bearer) {
+                const session = await getUserSession(bearer, authClient);
+
+                if (session?.data) {
+                    setSession(session.data);
+                } else {
+                    //? If session retrieval fails, redirect to login
+                    setSession(null);
+                    push("/login?sessionInvalid=true");
+                }
+            } else {
+                //? If no bearer token is found, redirect to login
+                setSession(null);
+                push("/login");
+            }
+        };
+
+        loadSession();
+    });
+
+    return (
+        <SessionContext.Provider value={{ session, setSession, logout }}>
+            {children}
+        </SessionContext.Provider>
+    );
+};
+
+export const useSessionContext = () => {
+    const context = useContext(SessionContext);
+
+    if (context === null) {
+        throw new Error(
+            "useSessionContext must be used within a SessionProvider",
+        );
+    }
+
+    return context;
+};
---
+++ apps/ui/stratify-ui/app/(screens)/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import "../../globals.css";
+import Providers from "../../global/Providers";
+import { getFontClassNames } from "@/lib/fonts";
+import { Toaster } from "@/app/components/ui/sonner";
+import { SessionProvider } from "./SessionProvider";
+
+export const metadata: Metadata = {
+    title: "Stratify UI",
+    description: "Stratify UI",
+};
+
+interface AuthLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function AppLayout({ children }: AuthLayoutProps) {
+    const apiProxyUrl = process.env.NEXT_PUBLIC_API_PROXY_URL || "";
+    const authProxyUrl = process.env.NEXT_PUBLIC_AUTH_PROXY_URL || "";
+    const fontClassNames = getFontClassNames();
+
+    return (
+        <html lang="en">
+            <head />
+            <body className={`${fontClassNames} antialiased`} id="root">
+                <Providers
+                    apiProxyUrl={apiProxyUrl}
+                    authProxyUrl={authProxyUrl}
+                >
+                    <SessionProvider>{children}</SessionProvider>
+                    <Toaster richColors />
+                </Providers>
+            </body>
+        </html>
+    );
+}
