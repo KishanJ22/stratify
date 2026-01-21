054a6e4d0c8cd5008456d474f1463789ddd89eb0
kishan <kishanjethwa224@gmail.com>
2026-01-21 21:59:34 +0000

    add config for test logging and update mock app setup

--- apps/api/stratify-api/src/__mocks__/mockApp.ts
+++ apps/api/stratify-api/src/__mocks__/mockApp.ts
@@ -1,22 +1,37 @@
-import fastify, { FastifyInstance } from "fastify";
+import Fastify, { type FastifyError } from "fastify";
 import FastifyFormBody from "@fastify/formbody";
 import autoload from "@fastify/autoload";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "../routes/auth.js";
+import { UserDetails } from "../utils/decodeToken.js";
+import logger from "../logger.js";
+import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import fastifyRequestContext from "@fastify/request-context";
+
+declare module "@fastify/request-context" {
+    interface RequestContextData {
+        requestId: string;
+        user: UserDetails | null;
+    }
+}
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const mockApp: FastifyInstance = fastify();
+const mockApp = Fastify({
+    disableRequestLogging: true,
+    genReqId: () => crypto.randomUUID(),
+}).withTypeProvider<TypeBoxTypeProvider>();
 
+mockApp.register(fastifyRequestContext);
 mockApp.register(FastifyFormBody);
-
-await mockApp.register(authRoutes);
 
 await mockApp.register(autoload, {
     dir: join(__dirname, "../plugins"),
     ignoreFilter: /.*\.test\..*$/,
 });
+
+await mockApp.register(authRoutes);
 
 await mockApp.register(autoload, {
     dir: join(__dirname, "../routes"),
@@ -24,6 +39,27 @@ await mockApp.register(autoload, {
     routeParams: false,
     matchFilter: /\.(?:get|post|put|delete|patch|options|head|)\..*$/,
     ignoreFilter: /.*\.test\..*$/,
+});
+
+mockApp.setErrorHandler<FastifyError>((error, request, reply) => {
+    logger.error(
+        { err: error },
+        `Error in request: ${request.method} ${request.url}`,
+    );
+
+    if (error.validation) {
+        return reply
+            .status(400)
+            .send({ error: "Bad Request", message: "Validation failed" });
+    }
+
+    if (error.statusCode) {
+        return reply.status(error.statusCode).send({ error: error.name });
+    }
+
+    return reply.status(500).send({
+        error: "Internal Server Error",
+    });
 });
 
 const loadMockApp = async () => {
--- apps/api/stratify-api/src/logger.ts
+++ apps/api/stratify-api/src/logger.ts
@@ -44,7 +44,16 @@ export const loggerVariant = {
         },
     },
     test: {
-        level: "silent",
+        level: process.env.LOG_LEVEL ?? "debug",
+        transport: {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "HH:MM:ss Z",
+                ignore: "pid,hostname",
+                levelFirst: true,
+            },
+        },
     },
 };
 
