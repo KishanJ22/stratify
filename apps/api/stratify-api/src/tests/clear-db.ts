e6da4894167087670728c8aedbf523d776d9da31
kishan <kishanjethwa224@gmail.com>
2026-01-21 22:01:07 +0000

    update function for clearing db for each test

--- apps/api/stratify-api/src/tests/clear-db.ts
+++ apps/api/stratify-api/src/tests/clear-db.ts
@@ -2,9 +2,13 @@ import db from "../database/db.js";
 
 //? Delete all data from tables used in tests
 const clearDatabase = async () => {
-    await db
-        .deleteFrom(["stratify.portfolios", "stratify.trades", "auth.user"])
-        .execute();
+    try {
+        await db.deleteFrom("stratify.portfolios").execute();
+        await db.deleteFrom("stratify.trades").execute();
+        await db.deleteFrom("auth.user").execute();
+    } catch (error) {
+        console.error("Error clearing database:", error);
+    }
 };
 
 export default clearDatabase;
