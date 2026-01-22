1c059e3991a7335a749581018fa4ed8e1ef5bca3
KishanJ22 <kishanjethwa224@gmail.com>
2026-01-22 11:57:14 +0000

    add 5 second wait before doing test db healthcheck

--- apps/api/stratify-api/src/utils/test-db-healthcheck.ts
+++ apps/api/stratify-api/src/utils/test-db-healthcheck.ts
@@ -1,9 +1,13 @@
 import { sql } from "kysely";
 import db from "../database/db.js";
 
+//? Script to test the connection to the database for testing
 const dbHealthCheck = async () => {
     try {
+        console.log("Waiting 5 seconds for db to be ready");
+        await new Promise((resolve) => setTimeout(resolve, 5000));
         await sql`SELECT 1`.execute(db);
+
         console.log("Database connection is healthy.");
         process.exit(0);
     } catch (error) {
