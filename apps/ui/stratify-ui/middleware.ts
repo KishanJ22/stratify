b4db9c1a80494cb4780290f301bc23c3ff136ec7
KishanJ22 <kishanjethwa224@gmail.com>
2026-01-22 15:09:27 +0000

    Fix CORS issue in middleware and simplify middleware layer

--- apps/ui/stratify-ui/middleware.ts
+++ apps/ui/stratify-ui/middleware.ts
@@ -1,4 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getAccessTokenFromCookies } from "./lib/auth/get-access-token";
+
+//? Add URLs for deployed environments here
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
 
 const API_URL = process.env.STRATIFY_API_URL;
 const AUTH_API_URL = process.env.STRATIFY_AUTH_API_URL;
@@ -6,16 +10,19 @@ const AUTH_API_URL = process.env.STRATIFY_AUTH_API_URL;
 const proxyRequest = async (request: NextRequest) => {
     const requestPathname = request.nextUrl.pathname;
 
+    // Allow calls to health endpoint without proxying
     if (requestPathname.startsWith("/api/health")) {
         return NextResponse.next();
     }
 
-    const isAuthRequest = requestPathname.startsWith("/api/auth");
-
+    // Check if API_URL is defined
     if (!API_URL) {
         console.error("STRATIFY_API_URL is not defined");
         return new NextResponse("Internal Server Error", { status: 500 });
     }
+
+    // If the request is for an auth endpoint, check if AUTH_API_URL is defined
+    const isAuthRequest = requestPathname.startsWith("/api/auth");
 
     if (isAuthRequest) {
         if (!AUTH_API_URL) {
@@ -26,23 +33,39 @@ const proxyRequest = async (request: NextRequest) => {
 
     try {
         const { pathname, search } = request.nextUrl;
+
+        // Get origin and check if it's allowed for CORS
+        const origin = request.headers.get("origin") || "";
+        const isAllowedOrigin = allowedOrigins.includes(origin);
+
         const headers = new Headers(request.headers);
 
         // Determine the target URL based on the request type
         const targetBaseUrl = isAuthRequest ? AUTH_API_URL : API_URL;
+        // Change proxy path to actual API path
         const targetPath = pathname.replace(/^\/api/, "");
         const targetUrl = `${targetBaseUrl}${targetPath}${search}`;
 
-        headers.delete("host");
-        headers.delete("connection");
-        headers.delete("x-forwarded-host");
-        headers.delete("x-forwarded-proto");
-        headers.set("x-original-url", request.url);
+        // Add CORS header for allowed origin to prevent CORS issues
+        const responseHeaders = {
+            ...(isAllowedOrigin && {
+                "Access-Control-Allow-Origin": origin,
+            }),
+        };
+
+        if (!isAuthRequest) {
+            const token = await getAccessTokenFromCookies();
+
+            if (token) {
+                headers.set("Authorization", `Bearer ${token}`);
+            }
+        }
 
         return NextResponse.rewrite(targetUrl, {
             request: {
                 headers,
             },
+            headers: responseHeaders,
         });
     } catch (error) {
         console.error("Proxy error:", error);
