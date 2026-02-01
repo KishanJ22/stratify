f7e350bd252b338d44a890e0bcd322f5070d9d16
KishanJ22 <kishanjethwa224@gmail.com>
2026-02-01 20:45:45 +0000

    remove unnecessary utility functions

--- apps/api/stratify-data-api/src/utils/safe_nested_get.py
+++
@@ -1,7 +0,0 @@
-def safe_nested_get(obj, *keys):
-    for key in keys:
-        if isinstance(obj, dict) and key in obj:
-            obj = obj[key]
-        else:
-            return None
-    return obj
