b50d38c1dc00235463abcfa76a30ec8e19fdc496
KishanJ22 <kishanjethwa224@gmail.com>
2026-02-07 18:40:57 +0000

    add unit tests for market data endpoints

---
+++ apps/api/stratify-api/src/routes/data/market/_mocks/mock-data-response.ts
@@ -0,0 +1,54 @@
+export const mockDataResponse = {
+    data: {
+        data: [
+            {
+                symbol: "ABC.L",
+                marketState: "POSTPOST",
+                priceDetails: {
+                    currentPrice: 46.55,
+                    dayTradingActivity: {
+                        open: 46.55,
+                        high: 46.55,
+                        low: 46.55,
+                        close: 19.5886,
+                        volume: 40,
+                        change: 26.9614,
+                        changePercent: 137.63821,
+                    },
+                },
+            },
+            {
+                symbol: "DEF.L",
+                marketState: "POSTPOST",
+                priceDetails: {
+                    currentPrice: 11.76,
+                    dayTradingActivity: {
+                        open: 11.76,
+                        high: 11.76,
+                        low: 11.76,
+                        close: 5.44,
+                        volume: 333,
+                        change: 6.32,
+                        changePercent: 116.176476,
+                    },
+                },
+            },
+            {
+                symbol: "ABCD",
+                marketState: "POSTPOST",
+                priceDetails: {
+                    currentPrice: 0.45,
+                    dayTradingActivity: {
+                        open: 0.45,
+                        high: 0.45,
+                        low: 0.45,
+                        close: 0.22,
+                        volume: 30500,
+                        change: 0.22999999,
+                        changePercent: 104.54545,
+                    },
+                },
+            },
+        ],
+    },
+};
---
+++ apps/api/stratify-api/src/routes/data/market/most-active.get.test.ts
@@ -0,0 +1,159 @@
+import { beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
+import loadMockApp from "../../../__mocks__/mockApp.js";
+import { mockDataResponse } from "./_mocks/mock-data-response.js";
+import db from "../../../database/db.js";
+import { createUser } from "../../../tests/create-user.js";
+import type { TopAsset } from "./top-assets-schema.js";
+
+const mockMostActivesDataGet = vi.fn();
+
+const mockDataApiClient = {
+    GET: mockMostActivesDataGet,
+};
+
+vi.mock("../../../lib/api/data-api-client.js", () => ({
+    dataApiClient: () => mockDataApiClient,
+}));
+
+describe("GET /data/market/most-active", () => {
+    const devToken =
+        "Bearer Dev-eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NjkwMzQ3NTgsIm5hbWUiOiJ0ZXN0LXVzZXIiLCJlbWFpbCI6InRlc3QtdXNlckB0ZXN0LmNvbSIsImVtYWlsVmVyaWZpZWQiOmZhbHNlLCJpbWFnZSI6bnVsbCwiY3JlYXRlZEF0IjoiMjAyNi0wMS0yMVQyMDo1MjoxMC45MTFaIiwidXBkYXRlZEF0IjoiMjAyNi0wMS0yMVQyMDo1MjoxMC45MTFaIiwidXNlcm5hbWUiOiJ0ZXN0LXVzZXIiLCJkaXNwbGF5VXNlcm5hbWUiOiJ0ZXN0LXVzZXIiLCJ0d29GYWN0b3JFbmFibGVkIjpmYWxzZSwiaWQiOiJ0ZXN0LXVzZXIiLCJzdWIiOiJ0ZXN0LXVzZXIiLCJleHAiOjE3NjkwMzgzNTgsImlzcyI6Imh0dHA6Ly8xMjcuMC4wLjE6MjAwMCIsImF1ZCI6Imh0dHA6Ly8xMjcuMC4wLjE6MjAwMCJ9.4h0c9ETlknvzcvHhQULDfCx5OF0_nlo-S_j0BbXqrdQbkzigNUqeU2E3EC-2YeSBMwJk6ERGar_LJ-sBAtK9DQ";
+
+    let app: any;
+
+    beforeAll(async () => {
+        app = await loadMockApp();
+    });
+
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it("should return the most active assets in the market and a 200 status", async () => {
+        await createUser("test-user").execute();
+
+        mockMostActivesDataGet.mockResolvedValue(mockDataResponse);
+
+        await db
+            .insertInto("stratify.assets")
+            .values([
+                {
+                    symbol: "ABC",
+                    name: "ABC company",
+                    type: "STOCK",
+                    countryId: 223,
+                    currency: "GBP",
+                },
+                {
+                    symbol: "DEF",
+                    name: "DEF company",
+                    type: "STOCK",
+                    countryId: 223,
+                    currency: "GBP",
+                },
+                {
+                    symbol: "ABCD",
+                    name: "ABCD company",
+                    type: "STOCK",
+                    countryId: 224,
+                    currency: "USD",
+                },
+            ])
+            .execute();
+
+        const response = await app.inject({
+            method: "GET",
+            url: "/data/market/most-active",
+            headers: {
+                Authorization: devToken,
+                Accept: "application/json",
+            },
+        });
+
+        expect(response.statusCode).toBe(200);
+
+        const json = await response.json();
+        const data = json.data as TopAsset[];
+
+        expect(data).toHaveLength(3);
+        expect(data[0]).toMatchObject({
+            symbol: "ABC",
+            name: "ABC company",
+            currency: "GBP",
+            assetType: "STOCK",
+            marketState: "POSTPOST",
+            priceDetails: {
+                currentPrice: 46.55,
+                volume: 40,
+                priceChange: 26.9614,
+                priceChangePercent: 137.63821,
+            },
+        });
+    });
+
+    it("should return a 404 status if there are no most active assets", async () => {
+        await createUser("test-user").execute();
+
+        mockMostActivesDataGet.mockResolvedValue({ data: { data: [] } });
+
+        const response = await app.inject({
+            method: "GET",
+            url: "/data/market/most-active",
+            headers: {
+                Authorization: devToken,
+                Accept: "application/json",
+            },
+        });
+
+        expect(response.statusCode).toBe(404);
+
+        const json = await response.json();
+        expect(json).toMatchObject({ message: "noMostActiveAssets" });
+    });
+
+    it("should not return assets for which details cannot be found in the db", async () => {
+        await createUser("test-user").execute();
+
+        mockMostActivesDataGet.mockResolvedValue(mockDataResponse);
+
+        await db
+            .insertInto("stratify.assets")
+            .values([
+                {
+                    symbol: "ABC",
+                    name: "ABC company",
+                    type: "STOCK",
+                    countryId: 223,
+                    currency: "GBP",
+                },
+                {
+                    symbol: "DEF",
+                    name: "DEF company",
+                    type: "STOCK",
+                    countryId: 223,
+                    currency: "GBP",
+                },
+            ])
+            .execute();
+
+        const response = await app.inject({
+            method: "GET",
+            url: "/data/market/most-active",
+            headers: {
+                Authorization: devToken,
+                Accept: "application/json",
+            },
+        });
+
+        expect(response.statusCode).toBe(200);
+
+        const json = await response.json();
+        const data = json.data as TopAsset[];
+
+        expect(data).toHaveLength(2);
+
+        data.forEach((asset) => {
+            expect("ABCD").not.toBe(asset.symbol);
+        });
+    });
+});
