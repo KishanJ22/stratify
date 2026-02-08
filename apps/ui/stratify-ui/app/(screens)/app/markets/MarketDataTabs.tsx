104c7dff52ef6aa2f0a42812a904f069bf5924e5
KishanJ22 <kishanjethwa224@gmail.com>
2026-02-08 01:02:37 +0000

    replace string props with literal type for switching between tabs

--- apps/ui/stratify-ui/app/(screens)/app/markets/MarketDataTabs.tsx
+++ apps/ui/stratify-ui/app/(screens)/app/markets/MarketDataTabs.tsx
@@ -1,9 +1,11 @@
 import { Tabs, TabsList, TabsTrigger } from "@/app/components/ui/tabs";
 import { Dispatch, SetStateAction } from "react";
 
+export type MarketDataTab = "topGainers" | "topLosers" | "mostActive";
+
 interface MarketDataTabsProps {
-    selectedTab: string;
-    setSelectedTab: Dispatch<SetStateAction<string>>;
+    selectedTab: MarketDataTab;
+    setSelectedTab: Dispatch<SetStateAction<MarketDataTab>>;
 }
 
 const MarketDataTabs = ({
@@ -12,7 +14,12 @@ const MarketDataTabs = ({
 }: MarketDataTabsProps) => {
     return (
         <div className="mt-4">
-            <Tabs defaultValue={selectedTab} onValueChange={setSelectedTab}>
+            <Tabs
+                defaultValue={selectedTab}
+                onValueChange={(value) =>
+                    setSelectedTab(value as MarketDataTab)
+                }
+            >
                 <TabsList>
                     <TabsTrigger value="topGainers">Top Gainers</TabsTrigger>
                     <TabsTrigger value="topLosers">Top Losers</TabsTrigger>
