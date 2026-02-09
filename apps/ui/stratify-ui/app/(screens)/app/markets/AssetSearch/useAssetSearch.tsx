64a0282da54d94ff1496f0673214fcc2b494ce6c
KishanJ22 <kishanjethwa224@gmail.com>
2026-02-09 20:44:42 +0000

    Build search button with popover to display text input and asset items beneath the search

---
+++ apps/ui/stratify-ui/app/(screens)/app/markets/AssetSearch/useAssetSearch.tsx
@@ -0,0 +1,25 @@
+import { useKyClient } from "@/lib/api/ky-client";
+import { paths } from "@/openapi/types/stratify-api";
+import { useMutation } from "@tanstack/react-query";
+
+export type SearchAsset =
+    paths["/assets/search"]["post"]["responses"]["200"]["content"]["application/json"]["data"][number];
+
+export const useAssetSearch = (searchQuery: string) => {
+    const client = useKyClient();
+
+    const { data, isPending, error, mutateAsync } = useMutation({
+        mutationKey: ["assetSearch", searchQuery],
+        mutationFn: async () =>
+            client
+                .POST("/assets/search", { body: { query: searchQuery } })
+                .then((res) => res.data?.data),
+    });
+
+    return {
+        searchResults: data,
+        isSearching: isPending,
+        searchError: error,
+        search: mutateAsync,
+    };
+};
