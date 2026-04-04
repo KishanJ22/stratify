cbbc7bacce5d8854a65603a90e56998765f6a2a5
KishanJ22 <kishanjethwa224@gmail.com>
2026-04-04 13:20:13 +0100

    add unit tests for asset hooks

---
+++ apps/ui/stratify-ui/app/(screens)/app/assets/[assetId]/_mocks/mockCryptocurrencyAssetDetails.ts
@@ -0,0 +1,20 @@
+export const mockCryptocurrencyAssetDetails = {
+    id: 3,
+    name: "Bitcoin",
+    symbol: "BTC",
+    countryId: 224,
+    assetCurrency: "USD",
+    assetType: "CRYPTOCURRENCY",
+    marketState: "REGULAR",
+    industry: null,
+    sector: null,
+    dayTradingActivity: {
+        open: 44000,
+        close: 44500,
+        high: 46000,
+        low: 43000,
+        priceChange: 500,
+        priceChangePercent: 1.12,
+        tradingVolume: 35000,
+    },
+};
---
+++ apps/ui/stratify-ui/app/(screens)/app/assets/[assetId]/_mocks/mockStockAssetDetails.ts
@@ -0,0 +1,25 @@
+export const mockStockAssetDetails = {
+    id: 1,
+    name: "Apple Inc.",
+    symbol: "AAPL",
+    countryId: 1,
+    assetCurrency: "USD",
+    assetType: "STOCK",
+    marketState: "REGULAR",
+    industry: "Consumer Electronics",
+    sector: [
+        {
+            sector: "technology",
+            weight: 1,
+        },
+    ],
+    dayTradingActivity: {
+        open: 149.0,
+        close: 150.0,
+        high: 151.0,
+        low: 148.75,
+        priceChange: 1.75,
+        priceChangePercent: 1.18,
+        tradingVolume: 75000000,
+    },
+};
